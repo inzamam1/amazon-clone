e63a115695feda0d97d3a7c1f0dfd788f2a59c86
inzamam1 <inzamamsayyed10@gmail.com>
2020-10-20 15:31:39 +0530

    v-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+/*react-routes-route to pages without refreshing,it is must faster
+pages in format---localhost:3000/login*/
 function App() {
   return (
-    <div className="App">
-      <h1>hey</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Router path="/login">
+            <h1>login</h1>
+          </Router>
+          <Route path="/">
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
+//switch is used to map pages
+//whats the difference between route and router
 export default App;
