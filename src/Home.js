55eef8bd4f72ec1b45adf66c929f6809a0aca3a3
inzamam1 <inzamamsayyed10@gmail.com>
2020-10-21 15:47:54 +0530

    Added products components

--- src/Home.js
+++ src/Home.js
@@ -7,20 +7,57 @@ function Home() {
       <img
         className="home__image"
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-        alt=""
       />
-
       {/* Product with id,title*/}
-
-      {/* <Product
-        id="123"
-        title="A book of heroes"
-        price={11}
-        rating={5}
-        image=""
-      /> */}
-
+      <div className="home__row">
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+      </div>
       {/* product2 */}
+      <div className="home__row">
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+      </div>
+      <div className="product__row">
+        <Product
+          id="123"
+          title="A book of heroes"
+          price={11}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/41dr+Ay2zjL._SX326_BO1,204,203,200_.jpg"
+        />
+      </div>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,1 +1,29 @@
 import "./Product.css";
+import React from "react";
+//using values passed by home.js
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {/* Javascript function to iterate over ratings */}
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} />
+      <button>Add to cart</button>
+    </div>
+  );
+}
+
+export default Product;
