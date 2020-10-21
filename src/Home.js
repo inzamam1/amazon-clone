84ce2b2125a1c94ced9313e9645845b0d04ecb72
inzamam1 <inzamamsayyed10@gmail.com>
2020-10-21 14:16:37 +0530

    Added product containers without modifications.

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Home.css";
-
+import Product from "./Product";
 function Home() {
   return (
     <div className="home">
       <img
         className="home__image"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuVgJe7Mh5nfvNUb0Bcfv-KuzWZkcuZEOlYg&usqp=CAU"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt=""
       />
+
+      {/* Product with id,title*/}
+
+      {/* <Product
+        id="123"
+        title="A book of heroes"
+        price={11}
+        rating={5}
+        image=""
+      /> */}
+
+      {/* product2 */}
     </div>
   );
 }
